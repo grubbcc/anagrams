a24827bc656431c3b533cf28ceb9b99e1201273c
grubbcc <66321115+grubbcc@users.noreply.github.com>
2021-12-02 12:56:50 -0800

    Resize containers to reduce memory consumption

--- server/Node.java
+++ server/Node.java
@@ -1,19 +1,19 @@
 package server;
 
-/*
- * A node representing a unique combination of letters and possibly one or more anagrams,
- * each representing a valid dictionary word.
- *
- */
-
 import java.util.TreeMap;
 import java.util.HashSet;
 import java.util.HashMap;
 
+/**
+ * A node representing a unique combination of letters and possibly one or more anagrams,
+ * each of which is a valid dictionary word.
+ *
+ */
+
 public class Node {
 
-	final HashSet<String> anagrams = new HashSet<>();
-	final HashMap<String, String> definitions = new HashMap<>();
+	final HashSet<String> anagrams = new HashSet<>(2);
+	final HashMap<String, String> definitions = new HashMap<>(2);
 
 	final TreeMap<Character, Node> children = new TreeMap<>();
 
