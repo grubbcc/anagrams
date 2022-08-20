9178563e3207ca719d68528c19997aec4ac6797f
grubbcc <grubbcc@gmail.com>
2022-08-20 02:00:44 -0700

    Added highlight CSW/NWL-only words option

--- AnagramsJPro/src/main/java/client/SettingsMenu.java
+++ AnagramsJPro/src/main/java/client/SettingsMenu.java
@@ -5,6 +5,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.css.PseudoClass;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.input.TouchEvent;
@@ -18,11 +19,11 @@ import java.util.HashMap;
  * A menu for choosing preferences and saving them for future use
  *
  */
-
 class SettingsMenu extends PopWindow {
 
     private final AnagramsClient client;
     private final CheckBox soundChooser = new CheckBox("Play sounds");
+    private final CheckBox highlightChooser = new CheckBox("");
     private final ChoiceBox<String> lexiconChooser = new ChoiceBox<>(FXCollections.observableArrayList(AnagramsClient.lexicons));
     private final EnumMap<AnagramsClient.Colors, String> newColors;
     private final HashMap<AnagramsClient.Colors, SettingsMenu.ColorChooser> colorChoosers = new HashMap<>();
@@ -30,7 +31,6 @@ class SettingsMenu extends PopWindow {
     /**
      *
      */
-
     SettingsMenu(AnagramsClient client) {
         super(client.stack);
         this.client = client;
@@ -54,25 +54,36 @@ class SettingsMenu extends PopWindow {
         lexiconChooser.getSelectionModel().select(client.prefs.get("lexicon", "CSW21"));
         lexiconChooser.pseudoClassStateChanged(PseudoClass.getPseudoClass("mobile"), client.getWebAPI().isMobile());
         soundChooser.setSelected(client.prefs.getBoolean("play_sounds", true));
-        soundChooser.setPadding(new Insets(0,9,0,7));
+
+        GridPane.setHalignment(soundChooser, HPos.RIGHT);
+        soundChooser.setPadding(new Insets(0,10,0,0));
+
+        highlightChooser.setSelected(client.prefs.getBoolean("highlight_words", false));
+        highlightChooser.setPadding(new Insets(3,0,-2,0)); //needs testing
+        Label goldLabel = new Label("Highlight");
+        goldLabel.setBackground(new Background(new BackgroundFill(Color.GOLD, CornerRadii.EMPTY, new Insets(2,0,1,2))));
+        Label clearLabel = new Label("CSW/NWL-only words");
+        HBox highlightTool = new HBox(highlightChooser, goldLabel, clearLabel);
+        highlightTool.setBorder(new Border(new BorderStroke(Color.TRANSPARENT, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.FULL)));
 
         //buttons
         Button OKButton = new Button("Okay");
         Button CancelButton = new Button("Cancel");
         Button ApplyButton = new Button("Apply");
 
-        grid.add(lexiconLabel, 0, 0);
-        grid.add(lexiconChooser, 1, 0);
-        grid.add(soundChooser, 2, 0);
-        grid.add(new ColorChooser(AnagramsClient.Colors.MAIN_SCREEN), 0, 1, 3, 1);
-        grid.add(new ColorChooser(AnagramsClient.Colors.PLAYERS_LIST), 0, 2, 3, 1);
-        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_FOREGROUND), 0, 3, 3, 1);
-        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_BACKGROUND), 0, 4, 3, 1);
-        grid.add(new ColorChooser(AnagramsClient.Colors.CHAT_AREA), 0, 5, 3, 1);
-        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_CHAT), 0, 6, 3, 1);
-        grid.add(OKButton, 0, 7);
-        grid.add(CancelButton, 1, 7);
-        grid.add(ApplyButton, 2, 7);
+        grid.add(lexiconLabel, 0, 0, 1, 1);
+        grid.add(lexiconChooser, 1, 0, 1, 1);
+        grid.add(soundChooser, 2, 0, 1, 1);
+        grid.add(highlightTool, 0, 1, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.MAIN_SCREEN), 0, 2, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.PLAYERS_LIST), 0, 3, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_FOREGROUND), 0, 4, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_BACKGROUND), 0, 5, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.CHAT_AREA), 0, 6, 3, 1);
+        grid.add(new ColorChooser(AnagramsClient.Colors.GAME_CHAT), 0, 7, 3, 1);
+        grid.add(OKButton, 0, 8);
+        grid.add(CancelButton, 1, 8);
+        grid.add(ApplyButton, 2, 8);
 
         OKButton.setOnAction(e -> { applyChanges(); savePreferences(); hide(); });
         CancelButton.setOnAction(e -> {revertChanges(); hide();});
@@ -80,7 +91,7 @@ class SettingsMenu extends PopWindow {
         closeButton.setOnAction(e -> {revertChanges(); hide();});
 
         setContents(grid);
-        setMaxSize(340,350);
+        setMaxSize(340,380);
         setTitle("Settings");
 
         setId("settings-menu");
@@ -95,7 +106,6 @@ class SettingsMenu extends PopWindow {
     /**
      * Instructs client to apply any requested color changes.
      */
-
     private void applyChanges() {
         for(AnagramsClient.Colors color : newColors.keySet()) {
             client.colors.put(color, newColors.get(color));
@@ -106,10 +116,10 @@ class SettingsMenu extends PopWindow {
     /**
      * Saves current color configuration to preferences file.
      */
-
     private void savePreferences() {
         client.prefs.put("lexicon", lexiconChooser.getSelectionModel().getSelectedItem() + "");
         client.prefs.putBoolean("play_sounds", soundChooser.isSelected());
+        client.prefs.putBoolean("highlight_words", highlightChooser.isSelected());
         for(AnagramsClient.Colors color : client.colors.keySet()) {
             client.prefs.put(color.key, client.colors.get(color));
         }
@@ -118,10 +128,10 @@ class SettingsMenu extends PopWindow {
     /**
      * Restores colors to previous values.
      */
-
     private void revertChanges() {
         lexiconChooser.getSelectionModel().select(client.prefs.get("lexicon", "CSW21"));
         soundChooser.setSelected(client.prefs.getBoolean("play_sounds", true));
+        highlightChooser.setSelected(client.prefs.getBoolean("highlight_words", false));
         for (AnagramsClient.Colors color : AnagramsClient.Colors.values()) {
             client.colors.put(color, client.prefs.get(color.key, color.defaultCode));
             colorChoosers.get(color).setColor(client.colors.get(color));
@@ -132,7 +142,6 @@ class SettingsMenu extends PopWindow {
     /**
      *
      */
-
     private class ColorChooser extends GridPane {
 
         AnagramsClient.Colors color;
@@ -149,9 +158,7 @@ class SettingsMenu extends PopWindow {
         String colorCode;
 
         /**
-         *
          */
-
         ColorChooser(AnagramsClient.Colors color) {
             this.color = color;
             setColor(client.colors.get(color));
@@ -170,8 +177,8 @@ class SettingsMenu extends PopWindow {
         }
 
         /**
+         *
          */
-
         private void setColor(String colorCode) {
 
             R = Integer.valueOf(colorCode.substring(1, 3), 16);
@@ -191,12 +198,10 @@ class SettingsMenu extends PopWindow {
         /**
          *
          */
-
         private class ColorPane extends GridPane {
 
             /**
              */
-
             ColorPane() {
                 getStyleClass().add("color-pane");
                 add(textFieldR, 0, 0);
@@ -212,7 +217,6 @@ class SettingsMenu extends PopWindow {
 
             /**
              */
-
             private ChangeListener<String> textListener = new ChangeListener<>() {
 
                 @Override
@@ -247,14 +251,12 @@ class SettingsMenu extends PopWindow {
         /**
          *
          */
-
         private class ColorComboBox extends ComboBox<String> {
 
             String[] colorCodes = {"#cd5c5c", "#f08080", "#fa8072", "#e9967a", "#ffa07a", "#dc143c", "#ff0000", "#b22222", "#8b0000", "#ffc0cb", "#ffb6c1", "#ff69b4", "#ff1493", "#c71585", "#db7093", "#ffa07a", "#ff7f50", "#ff6347", "#ff4500", "#ff8c00", "#ffa500", "#ffd700", "#ffff00", "#ffffe0", "#fffacd", "#fafad2", "#ffefd5", "#ffe4b5", "#ffdab9", "#eee8aa", "#f0e68c", "#bdb76b", "#e6e6fa", "#d8bfd8", "#dda0dd", "#ee82ee", "#da70d6", "#ff00ff", "#ff00ff", "#ba55d3", "#9370db", "#663399", "#8a2be2", "#9400d3", "#9932cc", "#8b008b", "#800080", "#4b0082", "#6a5acd", "#483d8b", "#7b68ee", "#adff2f", "#7fff00", "#7cfc00", "#00ff00", "#32cd32", "#98fb98", "#90ee90", "#00fa9a", "#00ff7f", "#3cb371", "#2e8b57", "#228b22", "#008000", "#006400", "#9acd32", "#6b8e23", "#808000", "#556b2f", "#66cdaa", "#8fbc8b", "#20b2aa", "#008b8b", "#008080", "#00ffff", "#00ffff", "#e0ffff", "#afeeee", "#7fffd4", "#40e0d0", "#48d1cc", "#00ced1", "#5f9ea0", "#4682b4", "#b0c4de", "#b0e0e6", "#add8e6", "#87ceeb", "#87cefa", "#00bfff", "#1e90ff", "#6495ed", "#7b68ee", "#4169e1", "#0000ff", "#0000cd", "#00008b", "#000080", "#191970", "#fff8dc", "#ffebcd", "#ffe4c4", "#ffdead", "#f5deb3", "#deb887", "#d2b48c", "#bc8f8f", "#f4a460", "#daa520", "#b8860b", "#cd853f", "#d2691e", "#8b4513", "#a0522d", "#a52a2a", "#800000", "#ffffff", "#fffafa", "#f0fff0", "#f5fffa", "#f0ffff", "#f0f8ff", "#f8f8ff", "#f5f5f5", "#fff5ee", "#f5f5dc", "#fdf5e6", "#fffaf0", "#fffff0", "#faebd7", "#faf0e6", "#fff0f5", "#ffe4e1", "#dcdcdc", "#d3d3d3", "#c0c0c0", "#a9a9a9", "#808080", "#696969", "#778899", "#708090", "#2f4f4f", "#000000"};
 
             /**
              */
-
             private ColorComboBox() {
                 setCellFactory(lv -> new ColorCell());
                 getStyleClass().add("color-combo-box");
@@ -268,7 +270,6 @@ class SettingsMenu extends PopWindow {
             /**
              *
              */
-
             private class ColorCell extends ListCell<String> {
 
                 String colorCode;
@@ -279,7 +280,6 @@ class SettingsMenu extends PopWindow {
                 }
                 /**
                  */
-
                 @Override
                 public void updateItem(String color, boolean empty) {
 
