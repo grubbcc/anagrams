9c223338a9a94dc0307d1bf4187ba4dad4266aa2
grubbcc <grubbcc@gmail.com>
2022-09-12 00:00:57 -0700

    switch to AnimatedGifEncoder library

--- AnagramsJPro/src/main/java/client/SnapshotPane.java
+++ AnagramsJPro/src/main/java/client/SnapshotPane.java
@@ -1,17 +1,19 @@
 package client;
 
 import javafx.animation.*;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.Scene;
+
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
 import javafx.scene.image.WritableImage;
+
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
 
-import javax.imageio.stream.FileImageOutputStream;
-import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -26,7 +28,7 @@ class SnapshotPane extends GameWindowBase {
     final SimpleDoubleProperty progress = new SimpleDoubleProperty(0);
     final SimpleBooleanProperty finished = new SimpleBooleanProperty(false);
 
-    GifSequenceWriter writer;
+    AnimatedGifEncoder encoder = new AnimatedGifEncoder();
 
     /**
      *
@@ -34,8 +36,8 @@ class SnapshotPane extends GameWindowBase {
     SnapshotPane(AnagramsClient client, String gameID, String username, String minLength, int blankPenalty, String numSets, String speed, String lexicon, ArrayList<String[]> gameLog) {
         super(client, gameID, username, minLength, blankPenalty, numSets, speed, lexicon, gameLog);
 
-        double WIDTH = numSets.equals("3") ? 1100 : 1000;
-        double HEIGHT = numSets.equals("3") ? 650 : 630;
+        int WIDTH = numSets.equals("3") ? 1100 : 1000;
+        int HEIGHT = numSets.equals("3") ? 650 : 630;
 
         setPrefSize(WIDTH, HEIGHT);
         setMinSize(WIDTH, HEIGHT);
@@ -45,11 +47,21 @@ class SnapshotPane extends GameWindowBase {
         Stage stage = new Stage(StageStyle.TRANSPARENT);
         stage.setScene(scene);
 
-        try {
-            animate();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        encoder.setDelay(1000);
+        encoder.setQuality(20);
+        encoder.setRepeat(0 /* infinite */);
+
+        encoder.setSize(WIDTH, HEIGHT);
+        applyCss();
+        layout();
+        Platform.runLater(() -> {
+            try {
+                animate();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
     }
 
     /**
@@ -57,18 +69,20 @@ class SnapshotPane extends GameWindowBase {
      * save the resulting images in gif format, and download it.
      */
     void animate() throws IOException {
+
         final Duration frameDuration = Duration.millis(1000);
         Timeline animation = new Timeline(20);
 
         File tempFile = new File("tmp/" + gameID + ".gif");
 
-        ImageOutputStream output = new FileImageOutputStream(tempFile);
-        writer = new GifSequenceWriter(output, frameDuration, true);
+        encoder.start(tempFile.getAbsolutePath());
 
         for (int i = 0; i < gameLog.size(); i++) {
             final int finalI = i;
+
             animation.getKeyFrames().add(new KeyFrame(frameDuration.multiply(i + 1), e -> {
                 showPosition(gameLog.get(finalI));
+
                 progress.set((double) finalI / gameLog.size());
                 try {
                     capture();
@@ -79,13 +93,15 @@ class SnapshotPane extends GameWindowBase {
         }
 
         //display final frame first
-        animation.getKeyFrames().add(new KeyFrame(Duration.ZERO, e -> {
+        animation.getKeyFrames().set(0, new KeyFrame(Duration.ZERO, e -> {
+
             showPosition(gameLog.get(gameLog.size() - 1));
             try {
                 capture();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
+            System.out.println();
         }));
 
         animation.playFromStart();
@@ -94,8 +110,7 @@ class SnapshotPane extends GameWindowBase {
             try {
                 finished.set(true);
                 System.out.println("FINISHED!");
-                writer.close();
-                output.close();
+                encoder.finish();
                 client.getWebAPI().downloadURL(tempFile.toURI().toURL());
             } catch (IOException e) {
                 e.printStackTrace();
@@ -114,10 +129,10 @@ class SnapshotPane extends GameWindowBase {
         snapshot(null, fxImage);
         BufferedImage image = SwingFXUtils.fromFXImage(fxImage, null);
 
-        // Remove alpha-channel from buffered image:
-        BufferedImage imageRGB = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.OPAQUE);
-        Graphics2D graphics = imageRGB.createGraphics();
+        Graphics2D graphics = image.createGraphics();
         graphics.drawImage(image, 0, 0, null);
-        writer.writeToSequence(imageRGB);
+
+        encoder.addFrame(image);
+
     }
 }
