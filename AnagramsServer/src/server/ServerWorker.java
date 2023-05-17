fc9f7ece95efd48c34b1e31643c5d8e05d3a0f48
grubbcc <grubbcc@gmail.com>
2023-05-17 00:31:32 -0700

    Fixed git mistake

--- AnagramsServer/src/server/ServerWorker.java
+++ AnagramsServer/src/server/ServerWorker.java
@@ -1,28 +1,20 @@
 package server;
 
-<<<<<<< Updated upstream
-=======
 import org.json.JSONException;
 import org.json.JSONObject;
 
->>>>>>> Stashed changes
 import java.net.Socket;
 import java.io.*;
+import java.util.Optional;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 /**
-* Handles tasks for a client on the server side.
+* Handles tasks for a client on the server side including communication between server and client.
 */
-class ServerWorker extends Thread {
+class ServerWorker implements Runnable {
 
 	private final Socket clientSocket;
-<<<<<<< Updated upstream
-	private final Server server;
-	private String username = null;
-	private OutputStream outputStream;
-	private InputStream inputStream;
-	private BufferedReader reader;
-
-=======
 	private String username;
 	final Server server;
 	private boolean guest = true;
@@ -30,7 +22,6 @@ class ServerWorker extends Thread {
 	private InputStream inputStream;
 	private BufferedReader reader;
 	private UserData prefs;
->>>>>>> Stashed changes
 
 	/**
 	*
@@ -46,12 +37,12 @@ class ServerWorker extends Thread {
 	@Override
 	public void run() {
 
-		while(!interrupted()) {
+		while(!Thread.interrupted()) {
 			try {
 				handleClientSocket();
 			}
 			catch (IOException e) {
-				interrupt();
+				Thread.currentThread().interrupt();
 				System.out.println(username + " has disconnected.");
 			}
 		}
@@ -91,30 +82,21 @@ class ServerWorker extends Thread {
 	 *
 	 * @param username The new user's name
 	*/
-	private void handleLogin(String username) {
+	private void handleLogin(String username, boolean guest) {
 
 		//Prevents duplicate usernames
-<<<<<<< Updated upstream
-		ServerWorker duplicate = server.getWorker(username);
-		if(duplicate != null) {
-			duplicate.send("logoffplayer " + username);
-=======
 		Optional<ServerWorker> duplicate = server.getWorker(username);
 		duplicate.ifPresent(dupe -> {
 			dupe.send("logoffplayer", new JSONObject().put("name", username));
->>>>>>> Stashed changes
 			server.removeWorker(username);
 			System.out.println("Removing duplicate user: " + username);
-		}
+		});
 
 		prefs = new UserData(username, guest);
 		send("login", prefs.get());
 
 		this.username = username;
-<<<<<<< Updated upstream
-=======
 		this.guest = guest;
->>>>>>> Stashed changes
 		System.out.println("User logged in successfully: " + username);
 
 		for (String s : server.announcements) {
@@ -125,26 +107,13 @@ class ServerWorker extends Thread {
 			send(new JSONObject().put("cmd", "chat").put("msg", s));
 		}
 
-<<<<<<< Updated upstream
-		//notify new player of other players
-		synchronized (server.getUsernames()) {
-			for (String playerName : server.getUsernames()) {
-				send("loginplayer " + playerName);
-			}
-=======
 		for (ServerWorker worker : server.getWorkers()) {
 			send("userdata", worker.getPublicData());
->>>>>>> Stashed changes
 		}
 
 		//notify new player of games
 		synchronized (server.getGames()) {
 			for(Game game : server.getGames()) {
-<<<<<<< Updated upstream
-				send("addgame " + game.getGameParams());
-				for(String playerName : game.getPlayerList()) {
-					send("takeseat " + game.gameID + " " + playerName);
-=======
 				send(game.params.put("cmd", "addgame"));
 				for(String playerName : game.players.keySet()) {
 					send(new JSONObject()
@@ -152,20 +121,14 @@ class ServerWorker extends Thread {
 						.put("gameID", game.gameID)
 						.put("name", playerName)
 						.put("rating", game.players.get(playerName).getRating() + ""));
->>>>>>> Stashed changes
 				}
 				if(game.gameOver) {
 					send(new JSONObject().put("cmd","endgame").put("gameID", game.gameID).put("gamelog", game.gameLog));
 				}
 				else {
-<<<<<<< Updated upstream
-					for(String inactivePlayer : game.getInactivePlayers()) {
-						send("abandonseat " + game.gameID + " " + inactivePlayer);
-=======
 					for(Player player : game.players.values()) {
 						if(player.abandoned)
 							send(new JSONObject().put("cmd", "abandonseat").put("gameID", game.gameID).put("name", player.name));
->>>>>>> Stashed changes
 					}
 					if(game.paused) {
 						send(new JSONObject().put("cmd", "note").put("game", game.gameID).put("msg", "Game paused"));
@@ -179,9 +142,6 @@ class ServerWorker extends Thread {
 
 		//notify other players of the new player
 		server.addWorker(username, this);
-<<<<<<< Updated upstream
-		server.broadcast("loginplayer " + username);
-=======
 		server.broadcast("userdata", prefs.getPublicData());
 	}
 
@@ -190,7 +150,6 @@ class ServerWorker extends Thread {
 	 */
 	JSONObject getPublicData() {
 		return prefs.getPublicData();
->>>>>>> Stashed changes
 	}
 
 	/**
@@ -199,6 +158,13 @@ class ServerWorker extends Thread {
 	private void handleLogoff() throws IOException {
 		disconnect();
 		server.logoffPlayer(username);
+	}
+
+	/**
+	 *
+	 */
+	String getUsername() {
+		return username;
 	}
 
 	/**
@@ -213,55 +179,20 @@ class ServerWorker extends Thread {
 		clientSocket.close();
 
 		System.out.println(username + " has disconnected");
-<<<<<<< Updated upstream
-		interrupt();
-=======
 
 		Thread.currentThread().interrupt();
->>>>>>> Stashed changes
 	}
 	
 	/**
 	* Creates the game and informs all players
 	*/
-<<<<<<< Updated upstream
-	private void handleCreateGame(String[] params) {
-	
-		String gameID = params[1];
-		String gameName = params[2];
-		int maxPlayers = Integer.parseInt(params[3]);
-		int minLength = Integer.parseInt(params[4]);
-		int numSets = Integer.parseInt(params[5]);
-		int blankPenalty = Integer.parseInt(params[6]);
-		String lexicon = params[7];
-		String speed = params[8];
-		boolean allowChat = Boolean.parseBoolean(params[9]);
-		boolean allowWatchers = Boolean.parseBoolean(params[10]);
-		boolean hasRobot = Boolean.parseBoolean(params[11]);
-		int skillLevel = Integer.parseInt(params[12]);
-		
-		Game newGame = new Game(server, gameID, gameName, maxPlayers, minLength, numSets, blankPenalty, lexicon, speed, allowChat, allowWatchers, hasRobot);
-		server.broadcast("addgame " + gameID + " " + gameName + " " + maxPlayers + " " + minLength + " " + numSets + " " + blankPenalty + " " + lexicon + " " + speed + " " + allowChat + " " + allowWatchers + " " + "false");
-		newGame.addPlayer(this);
-		if(hasRobot)
-			newGame.addRobot(new Robot(newGame, skillLevel, server.getDictionary(lexicon), minLength, blankPenalty));
-
-		server.addGame(gameID, newGame);
-=======
 	private void handleCreateGame(JSONObject params) {
 		server.broadcast(params.put("cmd", "addgame"));
 		Game newGame = new Game(server, params);
 		newGame.addPlayer(new Player(newGame, username, prefs));
 		server.addGame(newGame.gameID, newGame);
->>>>>>> Stashed changes
 	}
 
-	/**
-	*
-	*/
-	String getUsername() {
-		return username;
-	}
 
 	/**
 	 * Listens for commands from this worker's client and responds appropriately.
@@ -275,34 +206,6 @@ class ServerWorker extends Thread {
 		String line;
 		while((line = reader.readLine()) != null) {
 			System.out.println("command received: " + line);
-<<<<<<< Updated upstream
-			String[] tokens = line.split(" ");
-
-			if (tokens.length > 0) {
-				String cmd = tokens[0];
-				Game game = tokens.length > 1 ? server.getGame(tokens[1]) : null;
-
-				if(game == null) {
-					switch (cmd) {
-						case "login" -> handleLogin(tokens[1]);
-						case "logoff" -> handleLogoff();
-						case "chat" -> {
-							server.logChat(line.replaceFirst("chat ", ""));
-							server.broadcast(line);
-						}
-						case "newgame" -> handleCreateGame(tokens);
-						case "lookup" -> {
-							WordTree tree = new WordTree(tokens[2], server.getDictionary(tokens[1]));
-							tree.generateJSON(tree.rootWord, tree.rootNode, 1);
-							send("json " + tree.jsonArray.toString());
-						}
-						case "shutdown" -> {
-							if(clientSocket.getInetAddress().isLoopbackAddress())
-								System.exit(0);
-						}
-						default -> System.out.println("Error: Command not recognized: " + line);
-					}
-=======
 			JSONObject json;
 			String cmd;
 			try {
@@ -332,7 +235,6 @@ class ServerWorker extends Thread {
 				case "updateprofile" -> {
 					prefs.setProfile(json.getString("profile"));
 					server.broadcast("userdata", getPublicData());
->>>>>>> Stashed changes
 				}
 
 				//game-related commands
@@ -350,21 +252,12 @@ class ServerWorker extends Thread {
 								game.doSteal(json.getString("shortPlayer"), json.getString("shortWord"), json.getString("longPlayer"), json.getString("longWord"));
 							}
 						}
-<<<<<<< Updated upstream
-						case "gamechat" -> game.notifyRoom(line);
-						case "joingame" -> game.addPlayer(this);
-						case "watchgame" -> game.addWatcher(this);
-						case "stopplaying" -> game.removePlayer(this.username);
-						case "stopwatching" -> game.removeWatcher(this.username);
-						case "findplays" -> send("plays " + game.gameID + " " + game.findPlays(Integer.parseInt(tokens[2])));
-=======
 						case "gamechat" -> game.notifyRoom(json);
 						case "joingame" -> game.addPlayer(new Player(game, username, prefs));
 						case "watchgame" -> game.addWatcher(this);
 						case "stopplaying" -> game.removePlayer(username);
 						case "stopwatching" -> game.removeWatcher(username);
 						case "findplays" -> send(game.findPlays(json.getInt("position")));
->>>>>>> Stashed changes
 						default -> System.out.println("Error: Command not recognized: " + line);
 					}
 				}
