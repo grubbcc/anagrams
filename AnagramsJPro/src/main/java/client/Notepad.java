e0d22df2e39f3c8f7d550f3f0bb6e743f6d4e66d
grubbcc <grubbcc@gmail.com>
2023-05-22 15:58:30 -0700

    New notepad class for taking notes during games

---
+++ AnagramsJPro/src/main/java/client/Notepad.java
@@ -0,0 +1,18 @@
+package client;
+
+import javafx.scene.layout.Pane;
+
+public class Notepad extends PopWindow {
+
+    /**
+     * @param container The Pane in which this window resides.
+     */
+    Notepad(Pane container) {
+        super(container);
+
+
+
+    }
+
+
+}
