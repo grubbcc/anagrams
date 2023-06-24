f2cd904dc40e7ac32228dcc637df488032e70184
grubbcc <grubbcc@gmail.com>
2023-06-23 20:59:35 -0700

    make center draggable

--- AnagramsJPro/src/main/java/client/Notepad.java
+++ AnagramsJPro/src/main/java/client/Notepad.java
@@ -1,9 +1,16 @@
 package client;
 
+import javafx.event.Event;
+import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
-public class Notepad extends PopWindow {
+/**
+ * A widget for taking notes during a game.
+ */
+class Notepad extends PopWindow {
 
     /**
      * @param container The Pane in which this window resides.
@@ -14,13 +21,20 @@ public class Notepad extends PopWindow {
         setMinWidth(300);
         setMinHeight(400);
         makeResizable();
+        setAsDragZone(this);
         AnchorPane.setLeftAnchor(this, container.getWidth() - 400);
         AnchorPane.setTopAnchor(this, 180.0);
         setTitle("Notepad");
+
         TextArea textArea = new TextArea();
         textArea.setPrefSize(300, 150);
-        getStylesheets().add("css/notepad.css");
+        textArea.addEventFilter(MouseEvent.ANY, event -> {
+            if(!(event.getTarget() instanceof Text || event.getTarget() instanceof ScrollBar || event.getTarget() instanceof StackPane)) {
+                Event.fireEvent(this, event);
+            }
+        });
         setContents(textArea);
+        getStylesheets().add("css/notepad.css");
 
     }
 
